d12ae9e34c2dd1794162820c89f0b55a8342ed1c
ramboldio <lukas@rambold.io>
2016-07-14 01:22:37 +0200

    update

---
+++ www/js/routes.js
@@ -0,0 +1,52 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+      .state('search', {
+    url: '/search',
+    templateUrl: 'templates/search.html',
+    controller: 'ctrl'
+  })
+
+  .state('welcome', {
+    url: '/welcome',
+    templateUrl: 'templates/welcome.html',
+    controller: 'ctrl'
+  })
+
+  .state('contacts', {
+    url: '/contacts',
+    templateUrl: 'templates/contacts.html',
+    controller: 'ctrl'
+  })
+
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'templates/categories.html',
+    controller: 'ctrl'
+  })
+
+  .state('chat', {
+    url: '/chat',
+    templateUrl: 'templates/chat.html',
+    controller: 'ctrl'
+  })
+  .state('lend_me', {
+    url: '/lend_me',
+    templateUrl: 'templates/lend_me.html',
+    controller: 'ctrl'
+  })
+
+$urlRouterProvider.otherwise('/welcome')
+
+
+
+});
